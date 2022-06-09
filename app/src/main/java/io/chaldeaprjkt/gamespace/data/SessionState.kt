7b761ed4b527757e051b090b57f64f357884cd04
cjh1249131356 <cjh1249131356@gmail.com>
2022-06-09 17:28:56 +0800

    GameSpace: Let's take advantage of reTicker
    * It looks good in landscape mode, why not use it as notification overlay?
      This brings back in-game notifications settings.
    
    Vhmit: Add pt-rBR translations
    
    Change-Id: I1165ed0036a9348d1b29a10af693bca06d1ef864
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/data/SessionState.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/data/SessionState.kt
@@ -24,5 +24,7 @@ data class SessionState(
     var autoBrightness: Boolean? = null,
     var headsUp: Boolean? = null,
     var threeScreenshot: Boolean? = null,
+    var headsUp: Boolean? = null,
+    var reTicker: Boolean? = null,
     var ringerMode: Int = AudioManager.RINGER_MODE_NORMAL,
 )
